50feeae5500af81ea3d3c54e54a829767d073513
mayilook <damian@descreen.pl>
2017-03-07 23:13:03 +0100

    add

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -1,10 +1,39 @@
 /* Szyfr Cezara */
 #include <cctype>
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+string encode(string, int);
 
 int main()
 {
-    for(int a = 0; a < 256; ++a) // use int (big enough for 256)
-        if(std::isprint(a)) // check if printable
-            std::cout << char(a) << " "; // print it as a char
+    for(int bajt = 0; bajt < 256; ++bajt)
+        if(isprint(bajt))
+            cout << char(bajt) << " ";
+    cout << endl << "szyfr: ";
+    string test="tata", crypted;
+    crypted = encode("tata~",3);
+    cout<<crypted;
+    cout<<endl;
+    return 0;
+}
+
+
+
+string encode(string Phrase, int key){
+    string Crypted = Phrase;
+    uint PhraseSize = Phrase.length();
+    cout << "rozmiar slowa: " << PhraseSize <<endl;
+    int Tab[PhraseSize], sum;
+
+    for(uint i = 0; i < Phrase.length(); i++){
+        Tab[i] = int(Crypted[i]);
+        cout << "int: " <<Tab[i] << " litera: " << Crypted[i] <<endl;
+        sum = Tab[i] + key;
+        Tab[i] = sum%=255;
+        Crypted[i] = char(Tab[i]);
+    }
+    return Crypted;
 }
