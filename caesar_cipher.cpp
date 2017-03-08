a97341d711e419ac41a76a2f90ab9a7ea172251c
mayilook <damian@descreen.pl>
2017-03-08 19:36:52 +0100

    add

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -9,12 +9,12 @@ string encode(string, int);
 
 int main()
 {
-    for(int bajt = 0; bajt < 256; ++bajt)
-        if(isprint(bajt))
-            cout << char(bajt) << " ";
+    for(int bajt = 32; bajt < 127; ++bajt)
+        cout << " bajt : " << bajt << char(bajt) << " ";//if(isprint(bajt))
+            
     cout << endl << "szyfr: ";
     string test="tata", crypted;
-    crypted = encode("tata~",3);
+    crypted = encode("tata}~",2);
     cout<<crypted;
     cout<<endl;
     return 0;
@@ -25,15 +25,22 @@ int main()
 string encode(string Phrase, int key){
     string Crypted = Phrase;
     uint PhraseSize = Phrase.length();
-    cout << "rozmiar slowa: " << PhraseSize <<endl;
-    int Tab[PhraseSize], sum;
+    cout << "Rozmiar slowa: " << PhraseSize <<endl;
+    int Tab[PhraseSize], sum, modulo;
 
     for(uint i = 0; i < Phrase.length(); i++){
-        Tab[i] = int(Crypted[i]);
-        cout << "int: " <<Tab[i] << " litera: " << Crypted[i] <<endl;
+        Tab[i] = Crypted[i] - '0';
+        cout << "Bajt: " << Tab[i] << " Litera: " << Crypted[i];
         sum = Tab[i] + key;
-        Tab[i] = sum%=255;
-        Crypted[i] = char(Tab[i]);
+        modulo = sum%127;
+        if (modulo < 32) {
+            Tab[i] = modulo + 33;
+        }else{
+            Tab[i] = modulo;
+        }
+        
+        cout << " Bajt przesuniety: " << Tab[i] <<endl;
+        Crypted[i] = Tab[i] + '0';
     }
     return Crypted;
 }
